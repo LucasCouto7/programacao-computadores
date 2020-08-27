4b4fe2c3051b4b2e9cf6cc0a4dfd1e8d46d844f4
Lucas Couto <lukinha.cintra7@gmail.com>
2020-08-26 22:02:45 -0300

    exe9 versão final

--- lista1-exe9.js
+++ lista1-exe9.js
@@ -1,11 +1,18 @@
 chefe()
 
-function chefe(){
+function chefe() {
     var numero = Number(prompt("Informe o número"))
 
-    eex9(numero)
+    exe9(numero)
 }
 
-function exe9(){
-    
+function exe9(num) {
+    var soma = 0
+    for (var divisor = num; divisor >= 1; divisor--) {
+        if (num % divisor == 0) {
+            soma = soma + divisor
+        }
+
+    }
+    console.log("A soma dos divisores de " + num + " é " + soma)
 }
