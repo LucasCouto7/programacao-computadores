b68ccdd8edfdd68d055473772892fa36e23161f6
Lucas Couto <lukinha.cintra7@gmail.com>
2020-08-26 21:35:40 -0300

    exercício 7 pronto mas sem cálculo da média

--- lista1-exe7.js
+++ lista1-exe7.js
@@ -1,1 +1,29 @@
-console.log("Exercício 7")
+function ex7() {
+    var num = 0
+    var soma = 0
+    var i = 0
+
+
+
+
+    do {
+        num = Number(prompt("Insira o número(informe num negativo para encerrar o programa"))
+        if (num > 0) {
+            soma = soma + num
+            i++
+        }
+
+    } while (num > 0)
+
+    var result = media(soma, i)
+    console.log("Média dos números:", result)
+}
+
+
+
+function media(soma, i) {
+    var media = soma / i
+    return media
+}
+
+ex7();
